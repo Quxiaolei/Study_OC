9eae14cbb5c9213b6ea9293858823cea255ace74
xiaolei <coder_xiaolei@163.com>
2016-07-26 13:46:40 +0800

    add shareExtension

---
+++ ShareMenu/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  ShareMenu
+//
+//  Created by Madis on 16/7/26.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
